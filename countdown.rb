7efc1c864a865a4e23b6a3c7c4e5f3dfa823f7cb
Keenah Vancampenhout <kvancamp@mail.com>
2020-07-28 19:48:28 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -3,9 +3,9 @@ def countdown
     while count > 0
       puts "#{count} SECOND(S)!"
       count -=1
-
+        "HAPPY NEW YEAR!"
     end
-    "HAPPY NEW YEAR!"
+    
   end
 
   def countdown_with_sleep(count)
